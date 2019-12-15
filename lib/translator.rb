1e2442fec1a6ae07e9d04492cff9d6aa798f83d9
Anna Lewis Dagnino <annaick1995@gmail.com>
2019-12-15 03:25:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,19 @@
-# require modules here
+require "yaml"
+#require "pry"
 
-def load_library
-  # code goes here
+def load_library(path)
+  #file = File.read(file_path)
+  YAML.load_file( path )
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emoticon_to_get)
+  emitocons = load_library(path)
+  japanese_emoticon
+  emoticons.each{|emoticon|
+    puts emoticon[
+  }
 end
 
-def get_english_meaning
+def get_english_meaning(path, japanese_emoticon)
   # code goes here
 end
