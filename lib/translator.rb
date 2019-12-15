4268fb5c365d85a31bdcd92cd976539e0ae7153b
Anna Lewis Dagnino <annaick1995@gmail.com>
2019-12-15 03:28:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,10 +7,7 @@ end
 
 def get_japanese_emoticon(path, emoticon_to_get)
   emitocons = load_library(path)
-  japanese_emoticon
-  emoticons.each{|emoticon|
-    puts emoticon
-  }
+  
 end
 
 def get_english_meaning(path, japanese_emoticon)
